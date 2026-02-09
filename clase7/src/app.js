c227e879dd164bde4116a88a24266181da2eff00
Javier Veron <javier.veron@gmail.com>
2026-02-09 10:03:22 -0300

    Clase 7

--- clase7/src/app.js
+++ clase7/src/app.js
@@ -2,6 +2,9 @@ import express from "express"
 
 const app = express();
 const port = 8080;
+app.use(express.json()); // Capturar información que viaje en el Body
+app.use(express.urlencoded({extended:true})); // Capturar información que viaje por la Url
+
 app.listen(port, () => {
     console.log("Servidor Activo: " + port);
 })
@@ -19,7 +22,7 @@ app.listen(port, () => {
     </html>`); // Devuelvo un contenido HTML
 }) */
 
-const productos = [
+let productos = [
     {id:1, nombre:"Coca Cola", precio:4000},
     {id:2, nombre:"Coca Cola Zero", precio:3800},
     {id:3, nombre:"Pepsi", precio:3900},
@@ -35,7 +38,7 @@ const productos = [
 }) */
 
  
-/* Desafio: Mi primera API de Productos */
+/* Desafio: Mi primera API de Productos (CRUD) */
 app.get("/", (request, response) => {
     response.send(`<html>
     <body>
@@ -46,11 +49,66 @@ app.get("/", (request, response) => {
     </html>`); // Devuelvo un contenido HTML
 })
 
-app.get("/productos", (request, response) => {
-    response.send(productos); // Devuelvo el array de productos
-})
+function limitArray(unArray, limit) {
+    const arrayOutput = [];
 
-app.get("/productos/:id", (request, response) => {
+    for (let i=0; i<limit; i++) {
+        if (i >= unArray.length) {
+            break;
+        }
+        
+        arrayOutput.push(unArray[i]);
+    }
+
+    return arrayOutput;
+}
+
+function sortArray(unArray, order) {
+    unArray.sort((idx, item) => {
+        if (order.toLowerCase() == "asc") {
+            if (idx.id > item.id) {
+                return 1
+            }
+
+            if (idx.id < item.id) {
+                return -1
+            }
+        } else if (order.toLowerCase() == "desc") {
+            if (idx.id > item.id) {
+                return -1
+            }
+
+            if (idx.id < item.id) {
+                return 1
+            }
+        }
+    })
+
+    return unArray;
+}
+// Obtengo todos los Productos (array de objetos)
+app.get("/productos", (request, response) => { // Request queries
+    //const queries = request.query;
+    /* const limit = request.query.limit;
+    const order = request.query.order; */ // Opción #1
+    const {limit, order} = request.query;
+    let arrayOutput;
+
+    if (limit && order) {
+        arrayOutput = limitArray(productos, limit);
+        arrayOutput = sortArray(arrayOutput, order);
+    } else if (limit && !order) {
+        arrayOutput = limitArray(productos, limit);
+    } else if (!limit && order) {
+        arrayOutput = sortArray(productos, order);
+    } else {
+        arrayOutput = productos;
+    }
+    
+    response.send(arrayOutput); // Devuelvo un JSON (array de productos)
+})
+// Obtengo solo 1 Producto (objeto)
+app.get("/productos/:id", (request, response) => { // Request params
     /* const params = request.params;
     const id = params.id; */
     //const id = request.params.id; // Opción #1
@@ -63,8 +121,63 @@ app.get("/productos/:id", (request, response) => {
     const producto = productos.find(item => item.id == id);
 
     if (producto) {
-        response.send(producto);
+        response.send(producto); // Devulevo un JSON (objeto de producto)
     } else {
         response.status(400).send({status:"error", message:"No existe el Producto #" + id});
     }
+})
+
+const generarId = () => {
+    return productos.length + 1;
+}
+
+// Crear un nuevo Producto
+app.post("/productos", (request, response) => { // Request body
+    /* const nombre = request.body.nombre; // Opción #1
+    const precio = request.body.precio; */
+    const {nombre, precio} = request.body; // Opción #2
+    const nuevoProducto = {id:generarId(), nombre:nombre, precio:precio};
+    productos.push(nuevoProducto);
+
+    response.status(201).send(nuevoProducto);
+})
+
+// Actualizar un Producto (existente)
+app.put("/productos/:id", (request, response) => {
+    const {id} = request.params; // Opción #2
+
+    if (!Number.isInteger(parseInt(id))) { // Validar mi parámetro Id
+        response.status(400).send({status:"error", message:"El ID debe ser un número entero!"});
+    }
+
+    const {nombre, precio} = request.body;
+    const producto = productos.find(item => item.id == id);
+
+    if (producto) {
+        producto.nombre = nombre;
+        producto.precio = precio;
+
+        response.send(producto);
+    } else {
+        response.status(400).send({status:"error", message:"No existe el Producto #" + id});
+    }    
+})
+
+// Eliminar un Producto (existente)
+app.delete("/productos/:id", (request, response) => {
+    const {id} = request.params; // Opción #2
+
+    if (!Number.isInteger(parseInt(id))) { // Validar mi parámetro Id
+        response.status(400).send({status:"error", message:"El ID debe ser un número entero!"});
+    }
+
+    const producto = productos.find(item => item.id == id);
+
+    if (producto) {// [1, 2, 3, 4] => 2 => [1, 3, 4]
+        productos = productos.filter(item => item.id != id);
+
+        response.send({status:"ok", message:"Se eliminó el Producto #" + id});
+    } else {
+        response.status(400).send({status:"error", message:"No existe el Producto #" + id});
+    }   
 })
